d5496360c2cf86f9e4660a15784772897318af1f
saritapal-123 <sarita.pal6448@gmail.com>
2026-07-02 15:51:13 +0530

    update

--- src/main/java/com/hbn/main/Main.java
+++ src/main/java/com/hbn/main/Main.java
@@ -9,12 +9,17 @@ import com.hibernate.config.Hibernateconfig;
 public class Main {
 
 	public static void main(String[] args) {
-		Employee e1 =new Employee("vikas","male",330000);
+	//	Employee e1 =new Employee("vikas","male",330000);
+		Employee e1 =new Employee();
 		
 		Session session = Hibernateconfig.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
-		session.persist(e1);
-		
+	
+		session.load(e1, 5);
+	//	Employee employee = session.find(Employee.class,155);
+		//System.out.println(employee);
+		System.out.println(e1);
+	
 
 
 		tx.commit();
