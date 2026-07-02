9feb7e1d09b787ee95bd637033adcc0b58fc5648
saritapal-123 <sarita.pal6448@gmail.com>
2026-07-02 18:33:09 +0530

    update

--- src/main/java/com/hbn/entity/Employee.java
+++ src/main/java/com/hbn/entity/Employee.java
@@ -1,9 +1,20 @@
 package com.hbn.entity;
 
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+
+
+
+
+
+
+
+
+
 @Entity
 public class Employee {
 	@Id
--- src/main/java/com/hbn/main/Main.java
+++ src/main/java/com/hbn/main/Main.java
@@ -1,24 +1,53 @@
 package com.hbn.main;
 
 import org.hibernate.Session;
+import java.util.List;
 
 import org.hibernate.Transaction;
+import org.hibernate.query.MutationQuery;
+import org.hibernate.query.Query;
+
 import com.hbn.entity.Employee;
 import com.hibernate.config.Hibernateconfig;
 
 public class Main {
 
 	public static void main(String[] args) {
-	//	Employee e1 =new Employee("vikas","male",330000);
+	
 		Employee e1 =new Employee();
 		
 		Session session = Hibernateconfig.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
-	
-		session.load(e1, 5);
-	//	Employee employee = session.find(Employee.class,155);
-		//System.out.println(employee);
-		System.out.println(e1);
+		// to get all the records from DB
+	//	Query query = session.createQuery("from Employee",Employee.class);
+	//	List list = query.getResultList();
+	//	System.out.println(list);
+		
+	//	to get records with pagination from DB
+	//	Query query = session.createQuery("from Employee",Employee.class);
+	//	query.setFirstResult(2);
+	//	query.setMaxResults(5);
+	//	List list = query.getResultList();
+	//	System.out.println(list);
+		
+		// to update the records DB
+	//	MutationQuery query = session.createMutationQuery("update Employee set name = :n where id = :i");
+	//	query.setParameter("n", "param");
+	//	query.setParameter("i", 3);
+	//	query.executeUpdate();
+		
+		// to delete all the records from DB
+	//	MutationQuery query = session.createMutationQuery("delete from Employee  where id = :i");
+	//		query.setParameter("i", 7);
+	//		query.executeUpdate();
+			
+			//aggrigation function
+			Query query1 = session.createQuery("select max(name)from Employee");
+			List list = query1.getResultList();
+			System.out.println(list);
+		
+		
+		
 	
 
 
